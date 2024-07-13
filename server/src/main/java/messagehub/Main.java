80bcb0de1415db92bba133285a55dddddbf07bf1
Raymond Kouch <raymond-kouch@hotmail.com>
2024-07-13 23:40:34 +1000

    changes

--- server/src/main/java/messagehub/Main.java
+++ server/src/main/java/messagehub/Main.java
@@ -22,6 +22,7 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Main.class, args);
+        System.out.printf("application name: %s%n", context.getApplicationName());
     }
 
     @GetMapping
